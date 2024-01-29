9ff0d43a70e149bae213c66e54b627e61f970cca
mogilevichdmitry <dmsmain@gmail.com>
2024-01-29 20:49:48 +0100

    feat: implement cli functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const allVariablesNames = Object.keys(process.env);
+
+  const RSS_VARS = allVariablesNames
+    .filter((name) => name.startsWith("RSS_"))
+    .map((name) => `${name}=${process.env[name]}`)
+    .join("; ");
+
+  console.log(RSS_VARS);
 };
 
 parseEnv();
